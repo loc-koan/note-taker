6efc8f170cc580c07d7f501cb4dee303ede19d12
loc-koan <loc.koan@yahoo.com>
2020-06-29 18:18:02 -0500

    routes working

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ const PORT = process.env.PORT || 4040; /* video says not to use 8080 in heroku*/
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+/* routes */ 
+app.get('/',function (req, res) {
+    res.sendFile(path.join(__dirname,'/index.html'));
+});
+app.get('/notes',function (req, res) {
+    res.sendFile(path.join(__dirname,'/notes.html'));
+});
+app.get('*',function (req, res) {
+    res.sendFile(path.join(__dirname,'/index.html'));
+});
 
 /* starts and listens to port */
 app.listen(PORT, function () {
